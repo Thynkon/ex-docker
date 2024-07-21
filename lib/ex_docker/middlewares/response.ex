826964e10629bee20e5ca7ec7e88b5f8586933e8
Thynkon <thynkon@protonmail.com>
2024-07-21 17:24:30 +0200

    Return extra error message from docker engine

--- lib/ex_docker/middlewares/response.ex
+++ lib/ex_docker/middlewares/response.ex
@@ -8,8 +8,17 @@ defmodule ExDocker.Middlewares.Response do
            env
            |> Tesla.run(next) do
       case result.status do
-        code when code in [100, 200, 300] -> result.body
-        code -> {:error, Status.reason_atom(code)}
+        code when code in [100, 200, 300] ->
+          result.body
+
+        code ->
+          code_msg = Status.reason_atom(code)
+
+          if Map.has_key?(result.body, "message") do
+            {:error, code_msg, result.body["message"]}
+          else
+            {:error, code_msg}
+          end
       end
     end
   end
